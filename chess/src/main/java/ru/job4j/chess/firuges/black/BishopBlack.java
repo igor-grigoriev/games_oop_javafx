16bd78bda797b8377c6a81cf99b63a2ce5192e4c
igor-grigoriev <grigoriev.i.v@mail.ru>
2022-08-25 20:07:11 +0300

    2. Каркас шахматной доски [#792] fix

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -38,10 +38,7 @@ public class BishopBlack implements Figure {
     }
 
     public boolean isDiagonal(Cell source, Cell dest) {
-        if (Math.abs(dest.getX() - source.getX()) == Math.abs(dest.getY() - source.getY())) {
-            return true;
-        }
-        return false;
+        return (Math.abs(dest.getX() - source.getX()) == Math.abs(dest.getY() - source.getY()));
     }
 
     @Override
